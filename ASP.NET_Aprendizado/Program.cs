194f980948de0702dacc3e15192e985e0f51f8d5
YeQioK <felipe.marins.batista2003@gmail.com>
2021-06-22 16:51:23 -0300

    Adicionar arquivos de projeto.

---
+++ ASP.NET_Aprendizado/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASP.NET_Aprendizado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
